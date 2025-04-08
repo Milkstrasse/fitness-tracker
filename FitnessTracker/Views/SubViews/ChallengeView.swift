b3fb26d5a614006d1aa323f453a2f9992adaaf9c
Milkstrasse <55480379+Milkstrasse@users.noreply.github.com>
2025-04-08 11:05:04 +0200

    Removed icons & animated avatar

--- FitnessTracker/Views/SubViews/ChallengeView.swift
+++ FitnessTracker/Views/SubViews/ChallengeView.swift
@@ -10,26 +10,11 @@ import SwiftUI
 struct ChallengeView: View {
     let user: User
     
-    @State var blink: String = ""
-    
-    func blink(delay: TimeInterval) {
-        DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
-            blink = "_blink"
-            
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                blink = ""
-                
-                let blinkInterval: Int = Int.random(in: 5 ... 10)
-                blink(delay: TimeInterval(blinkInterval))
-            }
-        }
-    }
-    
     var body: some View {
         HStack(spacing: 0) {
             ZStack {
                 LinearGradient(gradient: Gradient(colors: [Color("GradientStart"), Color("GradientEnd")]), startPoint: .top, endPoint: .bottom)
-                Image("avatar" + blink).resizable().scaleEffect(2.1).offset(x: 10, y: -5)
+                Image("avatar").resizable().scaleEffect(2.1).offset(x: 10, y: -5)
             }
             .frame(width: 60, height: 60).clipShape(RoundedRectangle(cornerRadius: 15))
             Triangle().fill(Color("Challenge")).frame(width: 10, height: 15).padding(.leading, 5).offset(y: -5)
@@ -48,10 +33,6 @@ struct ChallengeView: View {
             .frame(height: 60)
         }
         .padding(.all, 30)
-        .onAppear {
-            let blinkInterval: Int = Int.random(in: 5 ... 10)
-            blink(delay: TimeInterval(blinkInterval))
-        }
     }
 }
 
